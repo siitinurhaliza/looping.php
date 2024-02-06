dac21923ab9b807fe4b63c5453eaf7e49d6ba12c
unknown <sn301620@gmail.com>
2024-02-07 01:10:57 +0700

    belajar looping

---
+++ perulanganwhile.php
@@ -0,0 +1,8 @@
+<?php
+    $i =0;
+    while ($i >= 20) {
+        echo "$i - I love cooding ";
+        echo "<br />";
+        $i+=2;
+    }
+?>
---
+++ while.php
@@ -0,0 +1,9 @@
+<?php
+ $a=1;
+ while ($i = 20){
+    echo "$i - love coding";
+    echo "<br />";
+    $i+=2;
+ }
+ ?>
+ 
